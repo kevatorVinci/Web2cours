2dc072774c91eb9f9dcd45074c0787202cfebc09
Kevin-Akator-Vinci <kevin.akator@student.vinci.be>
2024-10-06 15:53:04 +0200

    erreur ex 6 delete end put

--- Exercices/ex1.6/routes/films.ts
+++ Exercices/ex1.6/routes/films.ts
@@ -119,5 +119,92 @@ router.delete(":id", (req, res) => {
   return res.json(deletedElements[0]);
 });
 
+router.patch("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const film = films.find((drink) => drink.id === id);
+  if (!film) {
+    return res.sendStatus(404);
+  }
+
+  const body: unknown = req.body;
+
+ if (
+     !body ||
+     typeof body !== "object" ||
+     ("title" in body && (typeof body.title !== "string" || !body.title.trim())) ||
+     ("director" in body && (typeof body.director !== "string" || !body.director.trim())) ||
+     ("duration" in body && (typeof body.duration !== "number" || body.duration <= 0))
+ ) {
+     return res.sendStatus(400);
+ }
+const updatedFilm = { ...film, ...body };
+
+
+  films[films.indexOf(updatedFilm)] = updatedFilm;
+
+  return res.send(updatedFilm);
+
+
+});
+
+router.put("/:id", (req, res) => {
+  const body: unknown = req.body;
+
+  if (
+    !body ||
+    typeof body !== "object" ||
+    !("title" in body) ||
+    !("director" in body) ||
+    !("duration" in body) ||
+    typeof body.title !== "string" ||
+    typeof body.director !== "string" ||
+    typeof body.duration !== "number" ||
+    !body.title.trim() ||
+    !body.director.trim() ||
+    body.duration <= 0 ||
+    ("budget" in body &&
+      (typeof body.budget !== "number" || body.budget <= 0)) ||
+    ("description" in body &&
+      (typeof body.description !== "string" || !body.description.trim())) ||
+    ("imageUrl" in body &&
+      (typeof body.imageUrl !== "string" || !body.imageUrl.trim()))
+  ) {
+    return res.sendStatus(400);
+  }
+
+  
+
+  const id = Number(req.params.id);
+
+  if (isNaN(id)) {
+    return res.sendStatus(400);
+  }
+
+  const indexOfFilmToUpdate = films.findIndex((film) => film.id === id);
+  
+  if (indexOfFilmToUpdate < 0) {
+    const newFilm = body as Film;
+
+    
+   
+  
+
+    const nextId = films.reduce((acc, film) => (film.id > acc ? film.id : acc), 0) + 1;
+
+    const addedFilm = { id: nextId, ...newFilm };
+
+    films.push(addedFilm);
+
+    return res.json(addedFilm);
+  }
+
+ 
+  const updatedFilm = { ...films[indexOfFilmToUpdate], ...body } as Film;
+
+  films[indexOfFilmToUpdate] = updatedFilm;
+
+  return res.send(updatedFilm);
+});
+
 
 export default router;

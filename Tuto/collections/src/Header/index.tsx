ee549abc4849e6430eee5e38905a24a81ba1254a
Kevin-Akator-Vinci <kevin.akator@student.vinci.be>
2024-10-14 20:30:59 +0200

    modules import

---
+++ Tuto/collections/src/Header/index.tsx
@@ -0,0 +1,24 @@
+import pizzaBackground from "./assets/images/pizza.jpg";
+import "./App.css";
+import "./Header.css";
+
+
+interface HeaderProps {
+    title: string;
+    version:number;
+  }
+
+const Header = (props:HeaderProps): JSX.Element => {
+    return (
+      <div className="page" style={{ backgroundImage: `url(${pizzaBackground})` }}>
+      <header>
+        <h1 className="animate__animated animate__bounce">{props.title}</h1>
+        <h4>Version: {props.version}</h4>
+      </header>
+      </div>
+    );
+  };
+
+  export default  Header;
+
+  
